d3c74964946713c6b7442098b7ac9d75820fca96
basile-dejuzeur <basile.desjuzeur@hec.edu>
2024-03-25 17:33:24 +0100

    ex 08 : fix incomplete

--- ex08/ft_ten_queens_puzzle.c
+++ ex08/ft_ten_queens_puzzle.c
@@ -1,73 +1,48 @@
 #include <unistd.h>
 
-/*We reformulate the problem,
-we need to print all the permutations of
-0123456789, combinatory ensures that there is 
-exactly 10! of them*/
+/*The idea is the following one :
+We will start from a 10*10 grid  filled with 1s. Indiccating
+that the queen can be placed in any of the 100 squares.
+When a queen is placed somewhere, all the diagonals is 
+set to 0, as it would be eaten by the queen.
+We then recursively call the function with the new grid
+and the new queen to place.
+When we reach the last queen, we print the grid.
+*/
 
-int ft_recursive_factorial(int nb);
-void ft_swap(int *a, int *b);
-void ft_putchar(int c);
-void generate_permutations(int *arr, int start, int end);
+// Prototypes of the functions that will be used
+void ft_putchar(char c);
+void ft_print_grid(int grid[10][10]);
+void ft_place_queen(int grid[10][10], int queen, int *count);
 
+// main function
 int ft_ten_queens_puzzle(void)
 {
-    int arr[10] = {0,1,2,3,4,5,6,7,8,9};
-    generate_permutations(arr, 0, 9);
-    return (ft_recursive_factorial(10));
-}
-
-void generate_permutations(int *arr, int start, int end)
-{
     int i;
+    int j;
+    int count;
+    int *pointer;
+    int grid[10][10];
 
     i = 0;
-    if (start == end)
+    j = 0;
+    count = 0;
+    pointer = &count;
+    while (i < 10)
     {
-        while (i < 10)
+        j = 0;
+        while (j < 10)
         {
-            ft_putchar(arr[i] + '0');
-            i++;
+            grid[i][j] = 1;
+            j++;
         }
+        i++;
     }
-    else
-    {
-        while (i <= end)
-        {
-            ft_swap(&arr[start], &arr[i]);
-            generate_permutations(arr, start + 1, end);
-            ft_swap(&arr[start], &arr[i]);
-            i++;
-        }
-    }
+    ft_place_queen(grid, 0, pointer);
 }
 
-int ft_recursive_factorial(int nb)
-{
-    int result;
-    int i;
-
-    result = 1;
-    i = 1;
-
-    if (nb < 0)
-        return (0);
-    if (nb == 0)
-        return (1);
-
-    result = nb * ft_recursive_factorial(nb -1);
-    return (result);
-}
-
-void ft_swap(int *a, int *b)
-{
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void ft_putchar(int c)
-{
-    write(1, &c, 1);
-}
+// given a grid, a number of queen to place 
+// searches for next queen to place
+void ft_place_queen(int grid[10][10], int nb_placed_queens, int *count)
+{   
+        
