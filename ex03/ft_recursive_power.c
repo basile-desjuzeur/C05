127e493760d210e34ffb952dd478cacc4169328d
basile-dejuzeur <basile.desjuzeur@hec.edu>
2024-03-25 18:09:16 +0100

    ex03 : fix, puissance 0

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -7,6 +7,8 @@ int	ft_recursive_power(int nb, int power)
 		return (0);
 	if (power == 0 && nb == 0)
 		return (1);
+	if (power == 0)
+		return (1);
 	result = nb * ft_recursive_power(nb, power - 1);
 	return (result);
 }
